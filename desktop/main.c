c0266cd7d415ccd25fad6d26d3749faf576dd705
Zane Post <zapost86@gmail.com>
2023-12-29 08:44:23 -0600

    esp32s3? and also UI

--- desktop/main.c
+++ desktop/main.c
@@ -20,20 +20,24 @@
 #include "player.h"
 #include "net/net_client.h"
 
-#define MAX_COLUMNS 10
+// #define MAX_COLUMNS 10
 
 LocalBean bean = { 0 };
 
 const char *serverIp = "172.233.208.111";
+
+typedef enum GameScreen { TITLE, GAMEPLAY } GameScreen;
 
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
 int main(int argc, char *argv[])
 {
+    /* new ui time lets go
     if(argc >= 2) {
         serverIp = argv[1];
-    } 
+    }
+    */ 
     // Initialization
     //--------------------------------------------------------------------------------------
     const int screenWidth = 800;
@@ -51,7 +55,10 @@ int main(int argc, char *argv[])
     bean.cameraMode = CAMERA_FIRST_PERSON;
     UpdateCameraWithBean(&bean);
 
+    GameScreen currentScreen = TITLE;
+
     // Generates some random columns
+    /*
     float heights[MAX_COLUMNS] = { 0 };
     Vector3 positions[MAX_COLUMNS] = { 0 };
     Color colors[MAX_COLUMNS] = { 0 };
@@ -62,6 +69,7 @@ int main(int argc, char *argv[])
         positions[i] = (Vector3){ (float)(GetRandomValue(-15, 15)), heights[i]/2.0f, (float)(GetRandomValue(-15, 15)) };
         colors[i] = (Color){ (GetRandomValue(20, 255)), (GetRandomValue(10, 55)), 30, 255 };
     }
+    */
 
     bean.beanColor = (Color){ (GetRandomValue(0, 255)), (GetRandomValue(0, 255)), (GetRandomValue(0, 255)), (GetRandomValue(0, 255)) };
 
@@ -99,69 +107,99 @@ int main(int argc, char *argv[])
         }
         */
 
-        if(IsGamepadAvailable(0)) {
-            if(IsGamepadButtonPressed(0, GAMEPAD_AXIS_LEFT_TRIGGER)) {
-                if(bean.cameraMode != CAMERA_FIRST_PERSON) {
-                    bean.cameraMode = CAMERA_FIRST_PERSON;
-                    bean.up = (Vector3){ 0.0f, 1.0f, 0.0f }; // Reset roll
-                    UpdateCameraWithBean(&bean);
-                    //updateBeanCollide(&camera, cameraMode);
+        switch(currentScreen) {
+            case TITLE:
+            {
+                if(IsGamepadAvailable(0)) {
+                    if(IsGamepadButtonPressed(0, GAMEPAD_BUTTON_LEFT_THUMB)) {
+                        currentScreen = GAMEPLAY;
+                    }
+
+                    if(IsGamepadButtonPressed(0, GAMEPAD_BUTTON_RIGHT_THUMB)) {
+                        serverIp = "127.0.0.1";
+                        currentScreen = GAMEPLAY;
+                    }
+                } else {
+                    if(IsKeyPressed(KEY_ONE)) {
+                        currentScreen = GAMEPLAY;
+                    }
+
+                    if(IsKeyPressed(KEY_TWO)) {
+                        serverIp = "127.0.0.1";
+                        currentScreen = GAMEPLAY;
+                    }
                 }
+                break;
             }
-
-            if(IsGamepadButtonPressed(0, GAMEPAD_AXIS_RIGHT_TRIGGER)) {
-                if(bean.cameraMode != CAMERA_THIRD_PERSON) {
-                    bean.cameraMode = CAMERA_THIRD_PERSON;
-                    bean.up = (Vector3){ 0.0f, 1.0f, 0.0f }; // Reset roll
-                    UpdateCameraWithBean(&bean);
-                    //updateBeanCollide(&camera, cameraMode);
+            case GAMEPLAY:
+            {
+                if(IsGamepadAvailable(0)) {
+                    if(IsGamepadButtonPressed(0, GAMEPAD_AXIS_LEFT_TRIGGER)) {
+                        if(bean.cameraMode != CAMERA_FIRST_PERSON) {
+                            bean.cameraMode = CAMERA_FIRST_PERSON;
+                            bean.up = (Vector3){ 0.0f, 1.0f, 0.0f }; // Reset roll
+                            UpdateCameraWithBean(&bean);
+                            //updateBeanCollide(&camera, cameraMode);
+                        }
+                    }
+                    
+                    if(IsGamepadButtonPressed(0, GAMEPAD_AXIS_RIGHT_TRIGGER)) {
+                        if(bean.cameraMode != CAMERA_THIRD_PERSON) {
+                            bean.cameraMode = CAMERA_THIRD_PERSON;
+                            bean.up = (Vector3){ 0.0f, 1.0f, 0.0f }; // Reset roll
+                            UpdateCameraWithBean(&bean);
+                            //updateBeanCollide(&camera, cameraMode);
+                        }
+                    }
+                    
+                    if(IsGamepadButtonPressed(0, GAMEPAD_BUTTON_LEFT_THUMB)) {
+                        if(!client) {
+                            client = true;
+                            Connect(serverIp);
+                        }
+                    }
+                    
+                    if(IsGamepadButtonPressed(0, GAMEPAD_BUTTON_RIGHT_THUMB)) {
+                        bean.beanColor = (Color){ (GetRandomValue(0, 255)), (GetRandomValue(0, 255)), (GetRandomValue(0, 255)), (GetRandomValue(0, 255)) };
+                    }
                 }
-            }
-
-            if(IsGamepadButtonPressed(0, GAMEPAD_BUTTON_LEFT_THUMB)) {
-                if(!client) {
-                    client = true;
-                    Connect(serverIp);
+                
+                if ((IsKeyPressed(KEY_ONE))) {
+                    if(bean.cameraMode != CAMERA_FIRST_PERSON) {
+                        bean.cameraMode = CAMERA_FIRST_PERSON;
+                        bean.up = (Vector3){ 0.0f, 1.0f, 0.0f }; // Reset roll
+                        UpdateCameraWithBean(&bean);
+                        //updateBeanCollide(&camera, cameraMode);
+                    }
                 }
-            }
-
-            if(IsGamepadButtonPressed(0, GAMEPAD_BUTTON_RIGHT_THUMB)) {
-                bean.beanColor = (Color){ (GetRandomValue(0, 255)), (GetRandomValue(0, 255)), (GetRandomValue(0, 255)), (GetRandomValue(0, 255)) };
-            }
-        }
-
-        if ((IsKeyPressed(KEY_ONE)))
-        {
-            if(bean.cameraMode != CAMERA_FIRST_PERSON) {
-                bean.cameraMode = CAMERA_FIRST_PERSON;
-                bean.up = (Vector3){ 0.0f, 1.0f, 0.0f }; // Reset roll
-                UpdateCameraWithBean(&bean);
-                //updateBeanCollide(&camera, cameraMode);
-            }
-        }
-
-        if ((IsKeyPressed(KEY_TWO)))
-        {
-            if(bean.cameraMode != CAMERA_THIRD_PERSON) {
-                bean.cameraMode = CAMERA_THIRD_PERSON;
-                bean.up = (Vector3){ 0.0f, 1.0f, 0.0f }; // Reset roll
-                UpdateCameraWithBean(&bean);
-                //updateBeanCollide(&camera, cameraMode);
-            }
-        }
-
-        if ((IsKeyPressed(KEY_THREE)))
-        {
-            // setup client
-            if(!client) {
-                client = true;
+                
+                if ((IsKeyPressed(KEY_TWO))) {
+                    if(bean.cameraMode != CAMERA_THIRD_PERSON) {
+                        bean.cameraMode = CAMERA_THIRD_PERSON;
+                        bean.up = (Vector3){ 0.0f, 1.0f, 0.0f }; // Reset roll
+                        UpdateCameraWithBean(&bean);
+                        //updateBeanCollide(&camera, cameraMode);
+                    }
+                }
+                
+                if (IsKeyPressed(KEY_FOUR)) {
+                    // rng new color, might as well add this
+                    bean.beanColor = (Color){ (GetRandomValue(0, 255)), (GetRandomValue(0, 255)), (GetRandomValue(0, 255)), (GetRandomValue(0, 255)) };
+                }
+                    
                 Connect(serverIp); // bean game server dont ddos pls
-            }
-        }
 
-        if (IsKeyPressed(KEY_FOUR)) {
-            // rng new color, might as well add this
-            bean.beanColor = (Color){ (GetRandomValue(0, 255)), (GetRandomValue(0, 255)), (GetRandomValue(0, 255)), (GetRandomValue(0, 255)) };
+                if (Connected()) {
+                    connected = true;
+                    UpdateLocalBean(&bean);
+                } else if (connected) {
+                    // they hate us sadge
+                    Connect(serverIp);
+                    connected = false;
+                }
+                Update(GetTime(), GetFrameTime());
+                break;
+            }
         }
         
         if ((IsKeyPressed(KEY_FIVE)))
@@ -175,16 +213,6 @@ int main(int argc, char *argv[])
         }
 
         //UpdateLocalBean(&bean);
-        
-        if (Connected()) {
-            connected = true;
-            UpdateLocalBean(&bean);
-        } else if (connected) {
-            // they hate us sadge
-            Connect(serverIp);
-            connected = false;
-        }
-        Update(GetTime(), GetFrameTime());
 
         //----------------------------------------------------------------------------------
 
@@ -194,87 +222,76 @@ int main(int argc, char *argv[])
 
             ClearBackground(RAYWHITE);
 
-            BeginMode3D(bean.camera);
-
-                DrawPlane((Vector3){ 0.0f, 0.0f, 0.0f }, (Vector2){ 32.0f, 32.0f }, LIGHTGRAY); // Draw ground
-
-                if (!Connected()) {
-                    //DrawText("Connecting", 15, 75, 10, BLACK);
-                } else {
-                    //DrawText(TextFormat("Player %d", GetLocalPlayerId()), 15, 75, 10, BLACK);
-                    //printf("Bean %d position: x=%f, y=%f, z=%f\n", LocalPlayerId, bean.transform.translation.x, bean.transform.translation.y, bean.transform.translation.z);
-
-                    for (int i = 0; i < MAX_PLAYERS; i++) {
-                        if(i != GetLocalPlayerId()) {
-                            Vector3 pos = { 0 };
-                            uint8_t r;
-                            uint8_t g;
-                            uint8_t b;
-                            uint8_t a;
-                            if(GetPlayerPos(i, &pos) && GetPlayerR(i, &r) && GetPlayerG(i, &g) && GetPlayerB(i, &b), GetPlayerA(i, &a)) {
-                                DrawCapsule(
-                                    (Vector3){pos.x, pos.y + 0.2f, pos.z},
-                                    (Vector3){pos.x, pos.y - 1.0f, pos.z},
-                                    0.7f, 8, 8, (Color){ r, g, b, a }
-                                );
-                                DrawCapsuleWires(
-                                    (Vector3){pos.x, pos.y + 0.2f, pos.z},
-                                    (Vector3){pos.x, pos.y - 1.0f, pos.z},
-                                    0.7f, 8, 8, BLACK // an L color tbh
-                                );
+            switch(currentScreen) {
+                case TITLE: {
+                    DrawText("BEAN GAME", 20, 20, 40, BLACK);
+                    if(IsGamepadAvailable(0)) {
+                        DrawText("PRESS LEFT THUMB for MAIN SERVER, RIGHT THUMB for LOCAL SERVER", 120, 220, 20, BLACK);
+                    } else {
+                        DrawText("PRESS 1 for MAIN SERVER, 2 for LOCAL SERVER", 120, 220, 20, BLACK);
+                    }
+                }
+                case GAMEPLAY:
+                {
+                    BeginMode3D(bean.camera);
+                    
+                    DrawPlane((Vector3){ 0.0f, 0.0f, 0.0f }, (Vector2){ 32.0f, 32.0f }, LIGHTGRAY); // Draw ground
+                    
+                    if (!Connected()) {
+                        //DrawText("Connecting", 15, 75, 10, BLACK);
+                    } else {
+                        //DrawText(TextFormat("Player %d", GetLocalPlayerId()), 15, 75, 10, BLACK);
+                        //printf("Bean %d position: x=%f, y=%f, z=%f\n", LocalPlayerId, bean.transform.translation.x, bean.transform.translation.y, bean.transform.translation.z);
+                        
+                        for (int i = 0; i < MAX_PLAYERS; i++) {
+                            if(i != GetLocalPlayerId()) {
+                                Vector3 pos = { 0 };
+                                uint8_t r;
+                                uint8_t g;
+                                uint8_t b;
+                                uint8_t a;
+                                if(GetPlayerPos(i, &pos) && GetPlayerR(i, &r) && GetPlayerG(i, &g) && GetPlayerB(i, &b), GetPlayerA(i, &a)) {
+                                    DrawCapsule(
+                                        (Vector3){pos.x, pos.y + 0.2f, pos.z},
+                                        (Vector3){pos.x, pos.y - 1.0f, pos.z},
+                                        0.7f, 8, 8, (Color){ r, g, b, a }
+                                    );
+                                    DrawCapsuleWires(
+                                        (Vector3){pos.x, pos.y + 0.2f, pos.z},
+                                        (Vector3){pos.x, pos.y - 1.0f, pos.z},
+                                        0.7f, 8, 8, BLACK // an L color tbh
+                                    );
+                                }
                             }
                         }
                     }
+                    
+                    // Draw bean
+                    if (bean.cameraMode == CAMERA_THIRD_PERSON) {
+                        DrawCapsule(bean.topCap, bean.botCap, 0.7f, 8, 8, bean.beanColor);
+                        DrawCapsuleWires(bean.topCap, bean.botCap, 0.7f, 8, 8, BLACK);
+                        //DrawBoundingBox(beanCollide, VIOLET);
+                    }
+                    
+                    EndMode3D();
+
+                    // Draw info boxes
+                    DrawRectangle(5, 5, 330, 85, RED);
+                    DrawRectangleLines(5, 5, 330, 85, BLUE);
+                    DrawText("Player controls:", 15, 15, 10, BLACK);
+                    if(IsGamepadAvailable(0)) {
+                        DrawText("- Move: Left Analog Stick", 15, 30, 10, BLACK);
+                        DrawText("- Look around: Right Analog Stick", 15, 45, 10, BLACK);
+                        DrawText("- Camera mode: Left Trigger, Right Trigger", 15, 60, 10, BLACK);
+                        DrawText("- Generate a new color: Left Thumb", 15, 75, 10, BLACK);
+                    } else {
+                        DrawText("- Move keys: W, A, S, D, Space, Left-Ctrl", 15, 30, 10, BLACK);
+                        DrawText("- Look around: arrow keys or mouse", 15, 45, 10, BLACK);
+                        DrawText("- Camera mode keys: 1, 2", 15, 60, 10, BLACK);
+                        DrawText("- Generate a new color: 3", 15, 75, 10, BLACK);
+                    }
                 }
-                /*
-                DrawCube((Vector3){ -16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, BLUE);     // Draw a blue wall
-                DrawCube((Vector3){ 16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, LIME);      // Draw a green wall
-                DrawCube((Vector3){ 0.0f, 2.5f, 16.0f }, 32.0f, 5.0f, 1.0f, GOLD);      // Draw a yellow wall
-                */
-
-               // DrawLine3D(camera.position, camera.target, YELLOW);
-
-               /* This guy is just a test
-               DrawCapsule((Vector3){-3.0f, 2.2f, -3.0f}, (Vector3){-3.0f, 1.0f, -3.0f}, 0.7f, 8, 8, beanColor);
-               DrawCapsuleWires((Vector3){-3.0f, 2.2f, -3.0f}, (Vector3){-3.0f, 1.0f, -3.0f}, 0.7f, 8, 8, GREEN);
-               */
-
-                //DrawSphere(thatSphere.position, thatSphere.radius, thatSphere.color);
-                //DrawBoundingBox(thatSphere.sphereCollide, VIOLET);
-
-                /*
-                // Draw some cubes around
-                for (int i = 0; i < MAX_COLUMNS; i++)
-                {
-                    //DrawCube(positions[i], 2.0f, heights[i], 2.0f, colors[i]);
-                    //DrawCubeWires(positions[i], 2.0f, heights[i], 2.0f, MAROON);
-                }
-                */
-
-                // Draw point at target
-                //DrawCube(bean.target, 0.5f, 0.5f, 0.5f, YELLOW);
-                //DrawCubeWires(bean.target, 0.5f, 0.5f, 0.5f, DARKPURPLE);
-
-                // Draw bean
-                if (bean.cameraMode == CAMERA_THIRD_PERSON)
-                {
-                    DrawCapsule(bean.topCap, bean.botCap, 0.7f, 8, 8, bean.beanColor);
-                    DrawCapsuleWires(bean.topCap, bean.botCap, 0.7f, 8, 8, BLACK);
-                    //DrawBoundingBox(beanCollide, VIOLET);
-                }
-
-            EndMode3D();
-
-            // Draw info boxes
-            DrawRectangle(5, 5, 330, 100, RED);
-            DrawRectangleLines(5, 5, 330, 100, BLUE);
-
-            DrawText("Player controls:", 15, 15, 10, BLACK);
-            DrawText("- Move keys: W, A, S, D, Space, Left-Ctrl", 15, 30, 10, BLACK);
-            DrawText("- Look around: arrow keys or mouse", 15, 45, 10, BLACK);
-            DrawText("- Camera mode keys: 1, 2", 15, 60, 10, BLACK);
-            DrawText("- Generate a new color: 4", 15, 75, 10, BLACK);
-            DrawText("- Connect to network: 3", 15, 90, 10, BLACK);
+            }
 
         EndDrawing();
         //----------------------------------------------------------------------------------
